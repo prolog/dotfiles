811d7035e4dadeb260c82f6359076a6a03465e09
Julian Day <jcd748@mail.usask.ca>
2021-07-08 22:05:50 -0500

    Cleanup for 2021

--- tir/tir.tf
+++ tir/tir.tf
@@ -2,13 +2,11 @@
 ;
 ; Written by Tir <jcd748@mail.usask.ca>
 ; Mar 2007
+; Updated Jul 2021
 
 /echo Loading Aardwolf macros and triggers.
 
 /load tir/general.tf
-/load tir/checkresists.tf
-/load tir/sw.tf
-/load tir/spells.tf
 
 ; Define the connection to Aardwolf:
 /addworld Aardwolf aardmud.org 4010
